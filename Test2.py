c265edbbbc2c2662aa9a061949868e180ef8e8d5
Vendranm <36074846+Vendranm@users.noreply.github.com>
2018-03-09 17:02:14 +0100

    Test3.py

--- Test2.py
+++ Test2.py
@@ -2,6 +2,8 @@ from PIL import Image
 import numpy as np
 from resizeimage import resizeimage    
 import math
+import os
+
 def sobel(im):
     imConv = im.convert("L")
     buff =imConv.load()
@@ -30,7 +32,9 @@ def average_images(images):
         for y in range(images[0].size[1]): 
             mr,mg,mb=0,0,0
             for i in images:
-                pixi = i.load()
+                ic = i.convert("RGB")
+                #print(i.mode)
+                pixi = ic.load()
                 r,g,b = pixi[x,y]
                 mr +=r
                 mg +=g
@@ -41,41 +45,60 @@ def average_images(images):
             pixres[x,y]=R,G,B
     result.show()
     return result
-Taille=[32,64]
-im1 = Image.open('images/im1.jpeg')
-im1R = resizeimage.resize_contain(im1,Taille)
-im1S = sobel(im1R)
-im2 = Image.open('images/im2.jpg')
-im2R = resizeimage.resize_contain(im2,Taille)
-im2S = sobel(im2R)
-im3 = Image.open('images/im3.jpeg')
-im3R = resizeimage.resize_contain(im3,Taille)
-im3S = sobel(im3R)
-im4 = Image.open('images/im4.jpeg')
-im4R = resizeimage.resize_contain(im4,Taille)
-im4S = sobel(im4R)
-im5 = Image.open('images/im5.jpg')
-im5R = resizeimage.resize_contain(im5,Taille)
-im5S = sobel(im5R)
-im6 = Image.open('images/im6.jpg')
-im6R = resizeimage.resize_contain(im6,Taille)
-im6S = sobel(im6R)
-tabimR  =[im1R,im2R,im3R,im4R,im5R,im6R]
-tabimS = [im1S,im2S,im3S,im4S,im5S,im6S]
-imMoy = average_images(tabimS)
+def ouvImage():
+    Taille=[32,64]
+    tabimS = []
+    for dossier, sous_dossiers, fichiers in os.walk('images/faces94'):
+        for fichier in fichiers:
+            im = Image.open('images/faces94/fichier')
+            imC = im.convert("L")
+            imR = resizeimage.resize_contain(im,Taille)
+            imS = imR.sobel(imR)
+            tabimS.append(imS)
+    imMoy = average_images(tabimS)
+    imMoy.show()
+#Taille=[32,64]
+#im1 = Image.open('images/im1.jpeg')
+#im1C = im1.convert("RGB")
+#im1R = resizeimage.resize_contain(im1C,Taille)
+#im1S = sobel(im1R)
+#im2 = Image.open('images/im2.jpg')
+#im2C = im2.convert("RGB")
+#im2R = resizeimage.resize_contain(im2C,Taille)
+#im2S = sobel(im2R)
+#im3 = Image.open('images/im3.jpeg')
+#im3C = im3.convert("RGB")
+#im3R = resizeimage.resize_contain(im3C,Taille)
+#im3S = sobel(im3R)
+#im4 = Image.open('images/im4.jpeg')
+#im4C = im4.convert("RGB")
+#im4R = resizeimage.resize_contain(im4C,Taille)
+#im4S = sobel(im4R)
+#im5 = Image.open('images/im5.jpg')
+#im5C = im5.convert("RGB")
+#im5R = resizeimage.resize_contain(im5C,Taille)
+#im5S = sobel(im5R)
+#im6 = Image.open('images/im6.jpg')
+#im6C = im6.convert("RGB")
+#im6R = resizeimage.resize_contain(im6C,Taille)
+#im6S = sobel(im6R)
+#tabimR  =[im1R,im2R,im3R,im4R,im5R,im6R]
+#tabimS = [im1S,im2S,im3S,im4S,im5S,im6S]
+#imMoy = average_images(tabimS)
 
-def difference(im1,im2):
+def difference(im1,im2, dx, dy):
     marge =20
-    compris = false
+    compris = False
     pix1 = im1.load()
     pix2 = im2.load()
+    print(im1.mode, im2.mode)
     for x in range(im1.size[0]):
-        for y in range(im.size[1]):
+        for y in range(im1.size[1]):
             r,g,b = pix1[x,y]
-            R,G,B = pix2[x,y]
+            R,G,B = pix2[x+dx,y+dy]
             if abs(r-R)<=marge and abs(b-B)<=marge and abs(g-G)<=marge:
-                compris = true
-    if visage:
+                compris =True
+    if compris:
         print("Ceci est un visage") 
     return compris 
 
@@ -91,9 +114,11 @@ def comparaison(im):
         for y in range(0,H - hmoy):
             for i in range(lmoy):
                 for j in range(hmoy):
-                    difference(im,imMoy)
-                    x+=im
+                    difference(imMoy,im,x,y)
+                    x+=i
                     y+=j
+#comparaison(Image.open('images/imTest.JPG'))
+ouvImage()
 #for i in tabim:
     #i.show()    
     #print("Hauteur = ", i.size[0], "Largeur : ", i.size[1])
